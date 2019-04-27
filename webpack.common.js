a4c147e03ed183ba1dd5af0a83531d19f95648ee
Stephanus Andi Hermantyo <andihermantyo@gmail.com>
2019-04-27 21:15:01 +0700

    Add 'lint' npm script.

--- webpack.common.js
+++ webpack.common.js
@@ -3,24 +3,24 @@ const htmlWebpackPlugin = require('html-webpack-plugin');
 const htmlWebpackTemplate = require('html-webpack-template');
 
 module.exports = {
-	module: {
-		rules: [
-			{
+    module: {
+        rules: [
+            {
                 enforce: 'pre',
                 exclude: /node_modules/,
                 loader: 'eslint-loader',
                 test: /\.js$/
-			},
-			{
+            },
+            {
                 exclude: /node_modules/,
-				loader: 'babel-loader',
-				test: /\.js$/
-			}
-		]
-	},
-	output: {
-		filename: '[name].bundle.js'
-	},
+                loader: 'babel-loader',
+                test: /\.js$/
+            }
+        ]
+    },
+    output: {
+        filename: '[name].bundle.js'
+    },
     plugins: [
         new cleanWebpackPlugin(),
         new htmlWebpackPlugin({
--- webpack.dev.js
+++ webpack.dev.js
@@ -2,7 +2,7 @@ const common = require('./webpack.common.js');
 const webpackMerge = require('webpack-merge');
 
 module.exports = webpackMerge(common, {
-    devServer: {      
+    devServer: {
         host: '0.0.0.0'
     },
     mode: 'development'
