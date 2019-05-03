76a886854b576163393542177f66f839969cbaf6
Stephanus Andi Hermantyo <andihermantyo@gmail.com>
2019-05-03 13:03:27 +0700

    Remove previous installed files.

--- webpack.dev.js
+++
@@ -1,9 +0,0 @@
-const common = require('./webpack.common.js');
-const webpackMerge = require('webpack-merge');
-
-module.exports = webpackMerge(common, {
-    devServer: {
-        host: '0.0.0.0'
-    },
-    mode: 'development'
-});
